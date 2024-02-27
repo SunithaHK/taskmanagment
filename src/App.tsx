2a38e572bce409600992008fd87e178bb9ca2d8c
Sunitha_hk <sunithahk95@gmail.com>
2024-02-27 18:40:32 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
+    <div className='App'>
+      <header className='App-header'>
+        <div>Hi</div>
       </header>
     </div>
   );
